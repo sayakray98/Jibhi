11312cccbccba279e558abd650078bbe67a2db7d
Deepak Kumar <deepak@example.com>
2026-05-30 18:48:13 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+ plugins: [
+    react(),
+    viteImagemin({
+      mozjpeg: { quality: 75 },
+      optipng: { optimizationLevel: 5 },
+      pngquant: { quality: [0.7, 0.8] },
+      webp: { quality: 75 }
+    })
+  ],
   base: '/Jibhi/',
 })
